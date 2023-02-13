47b7a965ad37f6f92167466badd63e9239ef9988
Yeji <mnbvcxzyj@gmail.com>
2023-02-14 01:38:24 +0900

    #4. Login (1)

--- app.js
+++ app.js
@@ -7,22 +7,24 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(e) {
-  e.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  e.preventDefault(); // 새로고침 막기
+  loginForm.classList.add(HIDDEN_CLASSNAME); // form 숨기기
+  const username = loginInput.value; // 입력 값 변수로 저장
+  localStorage.setItem(USERNAME_KEY, username); // localStorage에 key, value 저장
+  paintGreetings(username); // 입력 값을 매개변수로 함수 호출
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${usernmae}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`; // 텍스트 추가
+  greeting.classList.remove(HIDDEN_CLASSNAME); // 클래스명 제거
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername == null) {
+  // localStorage에 이름이 비어 있으면
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
+  // 이름이 있으면
   paintGreetings(savedUsername);
 }
